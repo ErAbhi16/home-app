d83d8f733110c91910e6d1cb29397a51882a5756
Abhinav Goyal <abhinav.goyal@onsolve.com>
2022-03-26 16:18:21 +0530

    initial home setup

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EmptyRouteComponent } from './empty-route/empty-route.component';
+import { OverviewComponent } from './components/overview/overview.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EmptyRouteComponent,
+    OverviewComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
